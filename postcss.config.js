0cd399a8e34a78c242ab39f477705c0655faf158
Computer Help <no-reply@computerhelp.nl>
2025-07-03 00:51:42 +0200

    Convert PostCSS config to CommonJS for better compatibility

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,5 @@
 /** @type {import('postcss-load-config').Config} */
-const config = {
+module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
@@ -80,5 +80,3 @@ const config = {
     }),
   },
 };
-
-export default config;
