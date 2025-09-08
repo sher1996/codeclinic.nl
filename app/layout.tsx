1482444ef4c709bbf4078911b7868e2208c4d8b3
Computer Help <no-reply@computerhelp.nl>
2025-09-08 02:12:18 +0200

    Restore EmailAdminAccess component - test confirmed functionality works

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import { Inter } from 'next/font/google';
 import Script from 'next/script';
 import Header from '@/components/Header';
 import ErrorOverlay from '@/components/ErrorOverlay';
-import TestAdminAccess from '@/components/TestAdminAccess';
+import EmailAdminAccess from '@/components/EmailAdminAccess';
 import StructuredData from '@/components/StructuredData';
 import PerformanceMonitor from '@/components/PerformanceMonitor';
 
@@ -272,7 +272,7 @@ export default function RootLayout({
         </main>
         
         <ErrorOverlay />
-        <TestAdminAccess />
+        <EmailAdminAccess />
         <StructuredData />
         <PerformanceMonitor />
 
